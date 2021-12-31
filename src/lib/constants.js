70d209e9b0ceb7f74be03967f72946aa8c6bd24a
Justin Jeong <justin.jeong@buzzvil.com>
2021-12-31 23:33:50 +0900

    [005-LANDING] constants library

---
+++ src/lib/constants.js
@@ -0,0 +1,16 @@
+const CONSTANTS = {
+    BOOTSTRAP: {
+        VARIANT: [
+            "primary",
+            "secondary",
+            "success",
+            "warning",
+            "danger",
+            "info",
+            "light",
+            "dark",
+        ],
+    }
+}
+
+export default CONSTANTS;
