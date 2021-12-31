670e5e56233d3af77a2c02dbcd033657104df7f6
Justin Jeong <justin.jeong@buzzvil.com>
2021-12-31 23:34:09 +0900

    [005-LANDING] card.vue component for movie list

---
+++ src/components/cards/card.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <b-card :title="title"
+                :img-src="src"
+                :img-alt="alt"
+                img-top
+                :tag="tag"
+                style="max-width: 20rem;"
+                class="mb-2">
+            <b-card-text>
+                {{ description }}
+            </b-card-text>
+            <b-button :href="to"
+                      :variant="variant">
+                {{ buttonName }}
+            </b-button>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import OPTIONS from "lib/options";
+
+const { VARIANT } = OPTIONS;
+
+export default {
+    props: {
+        title: String,
+        src: String,
+        alt: {
+            type: String,
+            default: "Image",
+        },
+        tag: {
+            type: String,
+            default: "div",
+        },
+        description: String,
+        to: String,
+        buttonName: String,
+        variant: {
+            type: String,
+            default: "primary",
+            validator(v) {
+                return VARIANT[v];
+            },
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
