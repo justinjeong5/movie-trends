d08b6844f650847d8b27cfa179433c054efae3b3
Justin Jeong <justin.jeong@buzzvil.com>
2022-01-06 00:04:01 +0900

    [007-ROUTER] add vue-router dependency

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue"
 import App from "./App.vue"
+import VueRouter from 'vue-router'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
 
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import "style/style.scss";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
