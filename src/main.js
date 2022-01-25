a9ff8c1df4b4c61c1266cc0d97b62933e9e9694c
Justin Jeong <justin.jeong@buzzvil.com>
2022-01-25 15:28:24 +0900

    [007] linter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue"
 import App from "./App.vue"
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router"
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
 
 import "bootstrap/dist/css/bootstrap.css"
