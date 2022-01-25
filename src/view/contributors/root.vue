c5bdc2ac8de3d3474caf77b208f89b8d423d3b26
Justin Jeong <justin.jeong@buzzvil.com>
2022-01-25 23:20:33 +0900

    [007] page for contributors

---
+++ src/view/contributors/root.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Contributors</h1>
+        <div>TonyKang22</div>
+        <div>JustinJeong5</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
