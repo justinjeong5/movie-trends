b8fe1e384e20c5fc24b83ab2e7404c10029c3089
Justin Jeong <justin.jeong@buzzvil.com>
2022-01-25 23:16:36 +0900

    [007] rename store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import routingError from "./routingError";
+import routingErrors from "./routingErrors";
 import popularList from "./popularList";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        routingError,
+        routingErrors,
 
         popularList,
     },
