53c1dc364580bfc6d26a461cb90b313919e0c030
Justin Jeong <justin.jeong@buzzvil.com>
2022-01-25 23:19:41 +0900

    [007] init router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from "vue"
-import App from "./App.vue"
+import App from "./view/App.vue"
 import VueRouter from "vue-router"
+import store from "store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
-
-import router from "./view/router";
 
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
@@ -14,6 +13,7 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  store,
+  render: (h) => h(App),
+});
--- src/view/router.js
+++ src/view/router.js
@@ -1,35 +1,53 @@
-import store from "store";
 import VueRouter from "vue-router";
-import root from "view/movie/root.vue";
-import details from "view/movie/details.vue"
+import store from "store";
 
-const config = {
+import movieRoot from "view/movie/root.vue";
+import movies from "view/movie/movies.vue";
+import detail from "view/movie/detail.vue"
+
+import contributors from "view/contributors/root.vue"
+
+const router = new VueRouter({
+    mode: "history",
     routes: [
         {
-            path: "/",
+            path: "",
             name: "home",
-            component: root,
-            redirect: { name: "movie" },
+            redirect: { name: "movies" },
+            component: movieRoot,
             children: [
                 {
                     path: "movies",
-                    name: "movie",
-                    component: root,
-                    children: [
-                        {
-                            path: ":id",
-                            name: "detail",
-                            component: details,
-                        }
-                    ]
+                    name: "movies",
+                    component: movies,
                 },
+                {
+                    path: "movies/:id",
+                    name: "detail",
+                    component: detail,
+                }
             ]
         },
+        {
+            path: "/contributors",
+            name: "contributors",
+            component: contributors,
+        },
     ],
-};
-const router = new VueRouter(config);
+});
+
+router.beforeEach((to, from, next) => {
+    if (!to.matched.length) {
+        store.commit("setError", { data: { status: 404 } });
+        next();
+    } else {
+        next();
+        store.commit("setError", null);
+    }
+});
 
 router.onError((err) => {
+    console.error(err);
     store.commit("setError", { data: { status: 404 } });
     throw err;
 });
